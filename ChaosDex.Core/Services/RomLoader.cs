2ae349f6d7fd855cfd6d203175f2c6ece436005d
Aaron <psyap19@nottingham.ac.uk>
2026-07-10 18:04:53 +0100

    Implemented initial RomLoader - Loads a Rom from disk to memory - throws expection for invalid paths

--- ChaosDex.Core/Services/RomLoader.cs
+++ ChaosDex.Core/Services/RomLoader.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using ChaosDex.Core.Models;
+using System.Runtime.CompilerServices;
 
 namespace ChaosDex.Core.Services
 {
     public static class RomLoader
     {
-        public static void Load(string path)
+        public static Rom Load(string path)
         {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("ROM file not found:" + path);
+            }
+           
+            byte[] fileBytes = File.ReadAllBytes(path);
+            string fileName = Path.GetFileName(path);
 
+            return new Rom(fileName, path, fileBytes);
         }
 
     }
